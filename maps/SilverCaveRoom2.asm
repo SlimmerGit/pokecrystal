c65374b2438f95d9b6ad070ef6ec8b57c0554b52
SlimmerGit <seizgon@gmail.com>
2016-10-23 23:07:08 +0200

    Update SilverCaveRoom2.asm

--- maps/SilverCaveRoom2.asm
+++ maps/SilverCaveRoom2.asm
@@ -2,13 +2,50 @@ const_value set 2
 	const SILVERCAVEROOM2_POKE_BALL1
 	const SILVERCAVEROOM2_POKE_BALL2
 	const SILVERCAVEROOM2_POKE_BALL3
+	const SILVERCAVEROOM2_MEW
 
 SilverCaveRoom2_MapScriptHeader:
 .MapTriggers:
 	db 0
 
 .MapCallbacks:
-	db 0
+	db 1
+	
+	dbw MAPCALLBACKOBJECTS, .Mew
+
+.Mew:
+	checkevent EVENT_FOUGHT_MEW
+	iftrue .NoAppear
+	checkevent EVENT_BEAT_RED
+	iftrue .Appear
+	jump .NoAppear
+
+.Appear:
+	appear SILVERCAVEROOM2_MEW
+	return
+
+.NoAppear:
+	disappear SILVERCAVEROOM2_MEW
+	return
+
+Mew:
+	faceplayer
+	opentext
+	writetext MewText
+	cry MEW
+	pause 15
+	closetext
+	setevent EVENT_FOUGHT_MEW
+	writecode VAR_BATTLETYPE, BATTLETYPE_FORCEITEM
+	loadwildmon MEW, 40
+	startbattle
+	disappear SILVERCAVEROOM2_MEW
+	reloadmapafterbattle
+	end
+	
+MewText:
+	text "Meww!"
+	done	
 
 SilverCaveRoom2Calcium:
 	itemball CALCIUM
@@ -42,7 +79,8 @@ SilverCaveRoom2_MapEventHeader:
 	signpost 31, 14, SIGNPOST_ITEM, SilverCaveRoom2HiddenMaxPotion
 
 .PersonEvents:
-	db 3
+	db 4
 	person_event SPRITE_POKE_BALL, 10, 24, SPRITEMOVEDATA_ITEM_TREE, 0, 0, -1, -1, 0, PERSONTYPE_ITEMBALL, 0, SilverCaveRoom2Calcium, EVENT_SILVER_CAVE_ROOM_2_CALCIUM
 	person_event SPRITE_POKE_BALL, 24, 22, SPRITEMOVEDATA_ITEM_TREE, 0, 0, -1, -1, 0, PERSONTYPE_ITEMBALL, 0, SilverCaveRoom2UltraBall, EVENT_SILVER_CAVE_ROOM_2_ULTRA_BALL
 	person_event SPRITE_POKE_BALL, 20, 4, SPRITEMOVEDATA_ITEM_TREE, 0, 0, -1, -1, 0, PERSONTYPE_ITEMBALL, 0, SilverCaveRoom2PPUp, EVENT_SILVER_CAVE_ROOM_2_PP_UP
+	person_event SPRITE_MONSTER, 27, 11, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, (1 << 3) | PAL_OW_PINK,   PERSONTYPE_SCRIPT, 0, Mew, EVENT_SILVER_CAVE_MEW
